304f49f76766a87f8e23919df6d0875d75e2e812
Lokesh3011 <45530974+Lokesh3011@users.noreply.github.com>
2021-10-22 16:03:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, this is my second deployment');
+  res.send('Hello World, this is my another deployment');
 });
 
 app.listen(PORT, HOST);
